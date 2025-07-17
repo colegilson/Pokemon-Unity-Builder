95ab0902cc944e7547d93f59db97a903e8c3cf76
Cole Gilson <colegilson10@gmail.com>
2025-07-16 21:25:24 -0700

    more progress on builder, added helper functions, fixed formatting, and gathered more input

--- Pokemon-builder.py
+++ Pokemon-builder.py
@@ -1,7 +1,35 @@
 from dotenv import load_dotenv
+from enum import Enum
 import os
 
 dex_num = 0
+
+class TYPES(Enum):
+    NONE = 0
+    NORMAL = 1
+    FIRE = 2
+    WATER = 3
+    ELECTRIC = 4
+    GRASS = 5
+    ICE = 6
+    FIGHTING = 7
+    POISON = 8
+    GROUND = 9
+    FLYING = 10
+    PSYCHIC = 11
+    BUG = 12
+    ROCK = 13
+    GHOST = 14
+    DRAGON = 15
+    DARK = 16
+    STEEL = 17
+
+class GROWTH_RATES(Enum):
+    FAST = 0
+    MEDIUM_FAST = 1
+    MEDIUM_SLOW = 2
+    SLOW = 3
+
 load_dotenv()
 
 def dex_numbers() -> list:
@@ -11,21 +39,44 @@ def dex_numbers() -> list:
         dex_numbers.append(file[:3])
     return dex_numbers
 
+def find_next(cur_dex_num: int, dex_values: list) -> int:
+    while str(cur_dex_num) in dex_values:
+        cur_dex_num = int(cur_dex_num) + 1
+        cur_dex_num = f"{cur_dex_num:03d}"
+    return cur_dex_num
+
 if __name__ ==  '__main__':
     flag = True
     while flag:
         dex_values = dex_numbers()
+        
         if dex_num == 0:
             dex_num = int(input("What is the Pokemon's dex number?\n"))
             dex_num = f"{dex_num:03d}"
+            
+            if str(dex_num) in dex_values:
+                overwrite = input("This dex number exists and a file already exists for this mon, do you want to write over this file? [y/n]\n")
+            
+                if overwrite.lower().startswith("n"):
+                    dex_num = find_next(dex_num, dex_values)
+        
         else:
-            dex_num = int(dex_num) + 1
-            dex_num = f"{dex_num:03d}"
-            while str(dex_num) in dex_values:
-                dex_num = int(dex_num) + 1
-                dex_num = f"{dex_num:03d}"
-            print(dex_num)
-        pokemon_name = input("What is the Pokemon's name?\n")
+            dex_num = find_next(dex_num, dex_values) 
+            
+        print(f"Give information for Pokemon with Pokedex number {dex_num} please")
+        pokemon_name = input("What is this Pokemon's name?\n").lower().capitalize()
+        print(pokemon_name)
+        type_a = input("What is the primary type?\n").upper()
+        type_b = input("What is the primary type? ('none' for no secondary typing)\n").upper()
+        
+        for cur_type in TYPES:
+            if type(type_a) is str and type_a in str(cur_type):
+                type_a = cur_type.value
+            elif type(type_b) is str and type_b in str(cur_type):
+                type_b = cur_type.value
+                
+        catch_rate = input("What is the catchrate? 1-255\n")
+        exp_yield = input("How much exp do they yield? (fast, medium_fast, medium_slow, slow)\n").upper()
 
         is_done = input("are there others? [y/n]\n")
         if is_done.lower().startswith("n"):
