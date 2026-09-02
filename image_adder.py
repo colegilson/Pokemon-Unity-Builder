bcb5c6b7f5b5099ac7188ed86e5011a928343dce
Cole Gilson <colegilson10@gmail.com>
2026-09-01 21:57:32 -0700

    updated desired sprite, removed unused enum

--- image_adder.py
+++ image_adder.py
@@ -9,16 +9,19 @@ from io import BytesIO
 load_dotenv()
 BASEURL = "https://pokeapi.co/api/v2/pokemon/"
 
-class DIRECTION(Enum):
-    FRONT = 0
-    BACK = 1
-
 def main():
     for i in range(1, 387):
         response = requests.get(BASEURL+str(i)+"/").json()
 
-        back_sprite_url = response["sprites"]["back_default"]
-        front_sprite_url = response["sprites"]["front_default"]
+        try:
+            back_sprite_url = response["sprites"]['versions']['generation-iii']["firered-leafgreen"]["back_default"]
+            front_sprite_url = response["sprites"]['versions']['generation-iii']["firered-leafgreen"]["front_default"]
+            assert back_sprite_url != None
+
+        except:
+            back_sprite_url = response["sprites"]['versions']['generation-iii']["ruby-sapphire"]["back_default"]
+            front_sprite_url = response["sprites"]['versions']['generation-iii']["ruby-sapphire"]["front_default"]
+            
         back_sprite = Image.open(BytesIO(requests.get(back_sprite_url).content))
         front_sprite = Image.open(BytesIO(requests.get(front_sprite_url).content))
         
